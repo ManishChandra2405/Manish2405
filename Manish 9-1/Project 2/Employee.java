7b1217ec9ead44f6503c64a387c53116562d8480
ManishChandra2405 <59250434+ManishChandra2405@users.noreply.github.com>
2020-01-09 17:02:03 +0530

    Add files via upload

---
+++ Manish 9-1/Project 2/Employee.java
@@ -0,0 +1,38 @@
+package com.spring.web.entity;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Employee
+{
+	@Id
+	int empid;
+	String name;
+	double salary;
+	public Employee(){}
+	public Employee(int empid, String name, double salary)
+	{
+		this.empid = empid;
+		this.name = name;
+		this.salary = salary;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+
+}
